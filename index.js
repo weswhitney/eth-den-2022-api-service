2006dadd01973b6f88941a8b48728f236f20437c
weswhitney <wesleywhitney@gmail.com>
2022-02-14 10:12:43 -0700

    fix: port env for deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 // Initialize express and define a port
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 5000;
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
 // Start express on the defined port
