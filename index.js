e8c8cbcb00ecfdd1238a03079a79b5591bcf34a9
weswhitney <wesleywhitney@gmail.com>
2022-02-15 16:03:17 -0700

    fix: looks like webhook isnt set to hit hook route changing to root

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 // Start express on the defined port
 
 app.use(bodyParser.json())
-app.post("/hook", (req, res) => {
+app.post("/", (req, res) => {
   console.log('called')
   console.log(req.body) // Call your action on the request here
   res.status(200).end() // Responding is important
